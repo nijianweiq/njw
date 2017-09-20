27ef958a0d40bd0b87ff0fc3f6255c507388fed0
chenmenglan <chenmenglan@yazuo.com>
2017-09-20 11:17:03 +0800

    cml文件提交

---
+++ test/src/cml.java
@@ -0,0 +1,18 @@
+/**
+ * @file cml.java
+ * @project njw
+ * @copyright 无锡雅座在线科技发展有限公司
+ */
+
+/**
+ * 陈梦兰
+ * @author chenmenglan
+ * @create 2017-09-20
+ * @version 1.0
+ */
+public class cml {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
